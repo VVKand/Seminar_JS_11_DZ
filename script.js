38cfe5dffa007d30be09b7f6069bca268ce59fce
vkand <vvkand@mail.ru>
2023-04-01 11:23:01 +0700

    домашняя работа 12

--- script.js
+++ script.js
@@ -25,3 +25,31 @@ data.reverse().forEach((obj) => {
   </div>`
   );
 });
+
+const cardHove = document.querySelectorAll('.card__hov');
+const korzina = document.querySelector('.korzina');
+const obj = JSON.parse(database);
+
+cardHove.forEach(function(element, i) {
+  element.addEventListener("click", function() {
+korzina.insertAdjacentHTML('afterbegin',
+`
+<div class="korz__card">
+        <div class="korz__img">
+          <img src="${obj[i].img}" alt="photo" />
+        </div>
+        <div class="korz__content">
+          <h2>${obj[i].h5}</h2>
+          <p class="cont_par">
+            Price: <span class="korz__price">&nbsp;${obj[i].h6}</span>
+          </p>
+          <p class="cont_par">Color: <span class="dop">&nbsp;'Black'</span></p>
+          <p class="cont_par">Size: <span class="dop">&nbsp;'XL'</span></p>
+          <p class="cont_par">Quantity: <input type="number" value=1 /></p>
+
+          <button class="clouse" onclick="this.parentElement.parentElement.style.display='none';"><img src="/img/Vector (6).png" alt="" /></button>
+        </div>
+      </div>
+`)
+  });
+});
